e522dfd97d74d01180f4f6d63c40b2f3a3500efe
Tyler <tylerthomasallen@gmail.com>
2019-02-07 18:15:07 -0800

    Update blurb

---
+++ frontend/actions/index.js
@@ -0,0 +1,32 @@
+import { getProducts, getCart } from '../api';
+
+export const RECEIVE_PRODUCTS = 'RECEIVE_PRODUCTS';
+export const RECEIVE_CART = 'RECEIVE_CART';
+
+export const receiveProducts = payload => {
+    return {
+        type: RECEIVE_PRODUCTS,
+        payload
+    };
+};
+
+export const receiveCart = payload => {
+    return {
+        type: RECEIVE_CART,
+        payload
+    };
+};
+
+export const retrieveProducts = async () => dispatch => {
+    let products = await getProducts()
+    return(
+        dispatch(receiveProducts(products))
+    );
+};
+
+export const retrieveCart = async () => dispatch => {
+    let cart = await getCart();
+    return (
+        dispatch(receiveCart(cart))
+    );
+};
