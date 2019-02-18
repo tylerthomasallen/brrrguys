6540ec52d2b8592ee0734795111aae1fbd994ff4
Tyler <tylerthomasallen@gmail.com>
2019-02-18 15:49:03 -0800

    Update product title showing in email

--- app/services/cart_services/checkout.rb
+++ app/services/cart_services/checkout.rb
@@ -1,25 +1,25 @@
 module CartServices
-    class Checkout
-        def initialize(cart, update_params = {}, trigger_email = false, user_params = {})
-            @cart = cart
-            @update_params = update_params
-            @trigger_email = trigger_email
-        end
-
-        def execute
-            return false if @cart.nil?
-            products = @cart.products
-            @cart.products.delete_all
-            
-            if @trigger_email
-              UserMailer.checkout_mail(@update_params, products).deliver_now
-            end
-
-            true
-        end
-
-        def errors
-            @cart.errors.full_messages
-        end
+  class Checkout
+    def initialize(cart, update_params = {}, trigger_email = false, user_params = {})
+      @cart = cart
+      @products = cart.products
+      @update_params = update_params
+      @trigger_email = trigger_email
     end
+
+    def execute
+      return false if @cart.nil?
+      
+      if @trigger_email
+        UserMailer.checkout_mail(@update_params, @products).deliver_now
+      end
+      
+      @cart.products.delete_all
+      true
+    end
+
+    def errors
+      @cart.errors.full_messages
+    end
+  end
 end
